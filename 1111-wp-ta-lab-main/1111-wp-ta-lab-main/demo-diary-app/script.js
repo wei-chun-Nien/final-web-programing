11d2c368343bce3a7da2575566178587ab96d39b
wei-chun-Nien <116613597+wei-chun-Nien@users.noreply.github.com>
2022-12-24 21:59:44 +0800

    Update script.js

--- 1111-wp-ta-lab-main/1111-wp-ta-lab-main/demo-diary-app/script.js
+++ 1111-wp-ta-lab-main/1111-wp-ta-lab-main/demo-diary-app/script.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
